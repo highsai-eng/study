b93d6511d520909ddc996ec69364564b381c7cbe
kaito.higa <kaito.higa.free@gmail.com>
2023-06-06 13:55:27 +0900

    fix layouts

---
+++ nuxt-3/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header>Nuxt3アプリケーション</header>
+    <div class="container">
+      <slot />
+    </div>
+    <footer>© 2023 highsai-eng</footer>
+  </div>
+</template>
+
+<style scoped>
+header {
+  border-bottom-style: solid;
+  padding: 1rem;
+}
+footer {
+  margin-top: 2rem;
+  background-color: #8080EE;
+  padding: 1rem;
+}
+.container {
+  margin: 2rem;
+}
+</style>
---
+++ nuxt-3/pages/details.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const route = useRoute()
+const {id} = route.query
+
+const articles = [{
+  id: 1,
+  title: "Nuxt3入門",
+  content: "Nuxt3が公式リリースされました。Nuxt3ではVue3対応だけでなく、NitroやVite等様々な改善が施されています。"
+}, {
+  id: 2,
+  title: "Jest再入門",
+  content: "今回はJestのモックについて整理していきます。Jestはビルトインでマッチャーが提供され、これ単体で多くのユースケースをサポートします。"
+}]
+
+const article = ref<{id: number, title: string, content: string}>()
+article.value = articles.find(article => Number(id) === article.id)
+</script>
+
+<template>
+  <div>
+    <article v-if="article">
+      <p>タイトル：{{ article.title }}</p>
+      <hr />
+      <div style="width: 500px">{{ article.content }}</div>
+    </article>
+    <NuxtLink to="/">戻る</NuxtLink>
+  </div>
+</template>
